adb5b77a043c1e3c268fbc2c18d58f6d58b88b9b
Shaharear Emon <shaharearhossainemon@gmail.com>
2022-08-28 02:14:40 +0600

    first commit

---
+++ private/core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ROOT', 'http://localhost/Online_BRACU/public');
+define('ASSETS', 'http://localhost/Online_BRACU/public/assets');
+
+define('DBNAME', 'school_db');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBDRIVER', 'mysql');
